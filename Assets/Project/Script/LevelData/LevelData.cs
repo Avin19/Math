2c075d66d1e45c6789e620bca86f9c7074b7f48d
Avinash Yadav <avinashy1993@gmail.com>
2026-05-20 13:37:16 +0530

    Level Data

---
+++ Assets/Project/Script/LevelData/LevelData.cs
@@ -0,0 +1,14 @@
+
+using System;
+
+[Serializable]
+public class LevelData
+{
+    public int starEarned;
+    public bool unlocked;
+    public int Level;
+    public string Question;
+    public string Answer;
+    public string Category;
+    public string Difficulty;
+}
